c934bfc815bb8530db8cfb6b3130c76f30cff0dc
Gian Marco <gianmarcopablo588@gmail.com>
2024-04-02 23:55:38 -0500

    java first

---
+++ src/javaapplication1/Perro.java
@@ -0,0 +1,25 @@
+
+public class Perro {
+
+    String nombre;
+    String raza;
+    int edad;
+
+    void comer() {
+        System.out.println("El perro se encuentra comiendo");
+    }
+
+    void ladrar() {
+        System.out.println("El perro se encuentra comiendo");
+    }
+
+    void dormir() {
+        System.out.println("El perro se encuentra comiendo");
+    }
+
+    void establecerAtributos(String nombre, String raza, int edad) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.edad = edad;
+    }
+}
